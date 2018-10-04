d59a041ac1146430f74a469168b73e9038b2a0ab
imota <mota.icaro@gmail.com>
2018-10-04 19:41:45 -0400

    rotLeft problem solved.

---
+++ rotLeft.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+
+
+def rotLeft(a, d):
+    print(type(a[d:] + a[:d]))
+    return a[d:] + a[:d]
+
+if __name__ == '__main__':
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    print(result)
